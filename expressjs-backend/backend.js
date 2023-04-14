8953ba4ab3913387ebc0170d840f23fb915f216b
ysofian <ysofian@calpoly.edu>
2023-04-13 17:19:27 -0700

    first push

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 8000;
 
@@ -33,6 +34,7 @@ const users = {
     ]
  }
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/users/:id', (req, res) => {
